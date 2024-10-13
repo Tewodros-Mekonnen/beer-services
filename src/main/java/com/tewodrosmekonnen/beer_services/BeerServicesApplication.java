db9b114255bb84e119d6d4a7159abf268135ee63
tewodros-mekonnen <you@example.com>
2024-10-13 16:50:49 -0400

    Initial setup

---
+++ src/main/java/com/tewodrosmekonnen/beer_services/BeerServicesApplication.java
@@ -0,0 +1,13 @@
+package com.tewodrosmekonnen.beer_services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeerServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeerServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tewodrosmekonnen/beer_services/BeerServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tewodrosmekonnen.beer_services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
